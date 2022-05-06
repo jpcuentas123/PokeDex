877dd817181b37a7c33b0920a03e52bdcd86bc43
jpcuentas123 <33695061+jpcuentas123@users.noreply.github.com>
2022-05-06 07:30:46 -0500

    feat: screens for navigation

---
+++ src/navigations/index.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import pockedexLogo from '../../assets/pokeball.png';
+import FavoritesNavigation from './FavoritesNavigation';
+import AccountNavigation from './AccountNavigation';
+import PockedexNavigation from './PockedexNavigation';
+
+const Tab = createBottomTabNavigator();
+
+export default function MainNavigation() {
+  const ScreensList = {
+    Home: { focused: 'auto-awesome-mosaic', normal: 'auto-awesome-mosaic' },
+    Favorite: { focused: 'auto-awesome', normal: 'auto-awesome' },
+    Account: { focused: 'account-circle', normal: 'account-circle' },
+    Pokedex: { focused: 'blur-circular', normal: 'blur-circular' },
+  };
+
+  const getTabBarIcon = (name, focused, size, color) => {
+    let iconName;
+
+    // eslint-disable-next-line no-prototype-builtins
+    if (ScreensList.hasOwnProperty(name)) {
+      iconName = focused
+        ? ScreensList[`${name}`].focused
+        : ScreensList[`${name}`].normal;
+    }
+
+    if (name === 'Pokedex') {
+      return (
+        <Image
+          source={pockedexLogo}
+          // eslint-disable-next-line react-native/no-inline-styles
+          style={{ width: size * 3, height: size * 3, top: -15 }}
+        />
+      );
+    }
+    // You can return any component that you like here!
+    return (
+      <Icon name={iconName} size={size} color={focused ? color : '#263238'} />
+    );
+  };
+
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) =>
+          getTabBarIcon(route.name, focused, size, color),
+      })}
+      tabBarOptions={{
+        activeTintColor: '#2962ff',
+        inactiveTintColor: '#263238',
+      }}
+    >
+      <Tab.Screen
+        name="Favorite"
+        component={FavoritesNavigation}
+        options={{}}
+      />
+      <Tab.Screen
+        name="Pokedex"
+        component={PockedexNavigation}
+        options={{ tabBarLabel: '' }}
+      />
+      <Tab.Screen name="Account" component={AccountNavigation} options={{}} />
+    </Tab.Navigator>
+  );
+}
---
+++ src/utils/constants.js
@@ -0,0 +1,1 @@
+export const API_POCKEMONS_URL = 'https://pokeapi.co/api/v2/'
