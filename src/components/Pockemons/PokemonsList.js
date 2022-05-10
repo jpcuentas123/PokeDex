af82d197d4a2f4c994ed25c892b99440e492bec7
jpcuentas123 <33695061+jpcuentas123@users.noreply.github.com>
2022-05-10 07:57:43 -0500

    feat: pokemon list

--- src/components/Pockemons/PokemonsList.js
+++ src/components/Pockemons/PokemonsList.js
@@ -1,12 +1,22 @@
-import { Text, View } from 'react-native'
+import { FlatList, StyleSheet } from 'react-native'
 import React from 'react'
+import PokemonCard from './PokemonCard';
 
-const PokemonsList = () => {
+const PokemonsList = (props) => {
+    const { pokemons } = props;
     return (
-        <View>
-            <Text>PokemonsList</Text>
-        </View>
+        <FlatList data={pokemons} numColumns={3} showsVerticalScrollIndicator={false} keyExtractor={(pokemon) => String(pokemon.id)} renderItem={({ item }) =>
+            <PokemonCard pokemon={item} />
+        } contentContainerStyle={styles.flatListContentContainer} />
     )
 }
 
 export default PokemonsList
+
+const styles = StyleSheet.create({
+    flatListContentContainer: {
+        paddingHorizontal: 10,
+        paddingVertical: 10,
+
+    }
+})
